b2e69e8bedf7082027e639e1ddb74e46a4d0dd38
Rohan Sikder <Rohansikder4@gmail.com>
2021-10-20 18:53:37 +0100

    Added app Icon and changed sizing to android accomadate tablet

--- Yahtzee.Android/Resources/Resource.designer.cs
+++ Yahtzee.Android/Resources/Resource.designer.cs
@@ -13253,6 +13253,15 @@ namespace Yahtzee.Droid
 			// aapt resource value: 0x7F0C0002
 			public const int launcher_foreground = 2131492866;
 			
+			// aapt resource value: 0x7F0C0003
+			public const int yahtzeeicon = 2131492867;
+			
+			// aapt resource value: 0x7F0C0004
+			public const int yahtzeeicon_adaptive_back = 2131492868;
+			
+			// aapt resource value: 0x7F0C0005
+			public const int yahtzeeicon_adaptive_fore = 2131492869;
+			
 			static Mipmap()
 			{
 				global::Android.Runtime.ResourceIdManager.UpdateIdValues();
